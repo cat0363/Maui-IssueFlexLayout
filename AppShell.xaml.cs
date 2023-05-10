594e11ef9d021b1d94abb7a1e91a3f18c3f62ec9
cat0363 <kenji.nagano.0363@gmail.com>
2023-05-10 11:40:24 +0900

    Repro code of Maui FlexLayout Issue

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Maui_IssueFlexLayout;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Maui_IssueFlexLayout;
+
+public partial class MainPage : ContentPage
+{
+	ObservableCollection<TestItem> TestItems = new ObservableCollection<TestItem>();
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+        TestItems.Add(new TestItem() { ItemName = "a" });
+        TestItems.Add(new TestItem() { ItemName = "b" });
+        TestItems.Add(new TestItem() { ItemName = "c" });
+        TestItems.Add(new TestItem() { ItemName = "d" });
+        TestItems.Add(new TestItem() { ItemName = "e" });
+        TestItems.Add(new TestItem() { ItemName = "f" });
+        TestItems.Add(new TestItem() { ItemName = "g" });
+
+        BindableLayout.SetItemsSource(flTestItems, TestItems);
+    }
+}
+
+public class TestItem
+{
+	public string ItemName { get; set; }
+}
+
